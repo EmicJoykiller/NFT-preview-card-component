ac1996163f8a161ef2e67b2b8bf1f152ad49c5de
EmicJoykiller <152107345+EmicJoykiller@users.noreply.github.com>
2024-10-30 18:23:19 -0400

    Mobile design achived

--- src/App.js
+++ src/App.js
@@ -1,9 +1,45 @@
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      HI
+      <article className="card">
+        <img className="nft" src="/images/image-equilibrium.jpg" alt="nft" />
+
+        <div className="content-info">
+          <h1>Equilibrium #3429</h1>
+          <p>
+            Our Equilibrium collection promotes
+            <br />
+            balance and calm.
+          </p>
+        </div>
+        <div className="icons">
+          <div className="l-icon">
+            <img
+              className="ether-icon"
+              src="/images/icon-ethereum.svg"
+              alt="ether-icon"
+            />
+            <h1>0.041 ETH</h1>
+          </div>
+          <div className="r-icon">
+            <img
+              className="clock-icon"
+              src="/images/icon-clock.svg"
+              alt="clock-icon"
+            />
+            <h1>3 days left</h1>
+          </div>
+        </div>
+        <hr />
+        <div className="profile">
+          <img className="profile-img" src="/images/image-avatar.png" alt="profile-img" />
+          <p>
+            Creation of <span>Jules Wyvern</span>
+          </p>
+        </div>
+      </article>
     </div>
   );
 }
